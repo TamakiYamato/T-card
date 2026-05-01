81c99f201f2fbc85e60207774465729d9ef51efc
TamakiYamato <CA01244017@st.kawahara.ac.jp>
2026-05-01 10:04:20 +0900

    ファイルを整理

---
+++ Assets/Scripts/MVP/CardPresenter.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+/// <summary>
+/// Model‚ÆView‚ğŠÇ—‚·‚éƒNƒ‰ƒXB
+/// </summary>
+public class CardPresenter : MonoBehaviour
+{
+    /// <summary>
+    /// Start
+    /// </summary>
+    void Start()
+    {
+        
+    }
+
+    /// <summary>
+    /// Update
+    /// </summary>
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/MVP/CardView.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+/// <summary>
+/// カードの見た目を管理するクラス。
+/// </summary>
+public class CardView : MonoBehaviour
+{
+    /// <summary>
+    /// Start
+    /// </summary>
+    void Start()
+    {
+        
+    }
+
+    /// <summary>
+    /// Update
+    /// </summary>
+    void Update()
+    {
+        
+    }
+}
